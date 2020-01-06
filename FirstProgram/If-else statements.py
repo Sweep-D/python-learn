3e7f0b0b3f31929a4b553e83141ffb23596f4f02
sweep-d <32283724+Sweep-D@users.noreply.github.com>
2020-01-06 17:56:02 +1100

    if/else statements

---
+++ FirstProgram/If-else statements.py
@@ -0,0 +1,12 @@
+
+
+check = "Yo"
+
+if check == False:
+    print("It is false")
+elif check == "Hamburger":
+    print("Holy shit this tutorial, lol")
+elif check == "Yo":
+    print("Hello")    
+else:
+    print("It is actually equal to True")
