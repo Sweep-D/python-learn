199b35db3b393f535ba7c9b6aea0f345e4c02ad1
sweep-d <32283724+Sweep-D@users.noreply.github.com>
2020-01-07 19:56:01 +1100

    okay not this best calculator ever...

--- FirstProgram/calc.py
+++ FirstProgram/calc.py
@@ -7,17 +7,29 @@ print("Type 'quit' to exit\n")
 previous = 0
 run = True
 
+
 def performMath():
     global run
     global previous
-    equation = input("Enter equation:")
+    equation = ""
+
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+        equation = input(str(previous))
+
     if equation == 'quit':
+        print("Goodbye, human.")
         run = False
     else:
-        equation = re.sub('[A-Za-z,:;.()+" "]', '', equation)
+        equation = re.sub('[A-Za-z,:;.()" "]', '', equation)
+
+    if previous == 0:
         previous = eval(equation)
-        print("You typed:", previous)
+    else:
+        previous = eval(str(previous) + equation)
 
 
 while run:
     performMath()
+
