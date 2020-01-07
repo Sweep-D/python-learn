a23a7f0548cc9dc6996d227317c56d76726ee8ba
sweep-d <32283724+Sweep-D@users.noreply.github.com>
2020-01-07 21:24:40 +1100

    working out things with pep and linting

---
+++ PythonMisc/PepStuff.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+
+def my_function(one, two, three, four, five, six):
+    print("Hello World")
+
+
+def second_function():
+    print("Second Function")
+    print('Hello')
+
+
+my_list = [1, 2,
+           3, 4,
+           5, 6]
